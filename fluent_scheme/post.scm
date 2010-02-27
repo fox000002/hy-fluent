18d2ecd4b2cef6ae6eb3f8bf0352c66691f97501
fox000002 <fox000002@e86f901c-aaac-11de-ac14-117a7284e870>
2010-02-27 18:15:08 +0000

    Fixing post.scm.

--- fluent_scheme/post.scm
+++ fluent_scheme/post.scm
@@ -1,3 +1,24 @@
+; Copyright (C) 2009  huys, huys03@gmail.com
+;
+; This program is free software: you can redistribute it and;or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;
+;; post.scm
+;;
+;; The scheme script for postprocessing.
+;;
+
 (define casename "20100224_final_ft14000xc")
 (define zoneid 7)
 
@@ -39,10 +60,11 @@
   (ti-menu-load-string
     (format #f "plot/plot-direction 0 1")
   )
-
+  (newline)
   (ti-menu-load-string
     (format #f "plot/plot yes ~a yes no no temperature no no y-coordinate ~a" fname zone-id)
   )
+  (newline)
 )
 
 ;;
@@ -95,7 +117,7 @@
 )
 
 (define hy-write-list-tec-file
-  (lambda (fname ll t)
+  (lambda (fname ll tt)
     (let ((p (open-output-file (format #f "~a" fname))))
       (begin
         (display "VARIABLES = \"Time (s)\", \"Y (m)\", \"Temperature\"" p)
@@ -105,9 +127,9 @@
         (do ((i  0 (+ i 2)))
           ((>= i (length ll)))
           (begin
-            (display (format #f "~8a    ~8a    ~8a" t (list-ref ll i) (list-ref ll (+ i 1))) p)
+            (display (format #f "~8a  ~8a  ~8a" tt (list-ref ll i) (list-ref ll (+ i 1))) p)
             (newline p)
-            (display (format #f "~8a    ~8a    ~8a" (+ t 400) (list-ref ll i) (list-ref ll (+ i 1))) p)
+            (display (format #f "~8a  ~8a  ~8a" (+ tt 400) (list-ref ll i) (list-ref ll (+ i 1))) p)
             (newline p)
           )
         )
@@ -134,20 +156,22 @@
 
 (hy-write-list-tec-file
   (format #f "th~04d.dat" 0)
-    (hy-read-xyplot-file (format #f "th~04d.xy" 0) 0)
-  )
+  (hy-read-xyplot-file (format #f "th~04d.xy" 0))
+  0
 )
 
 (do ((i  20 (+ i 20)) (ctime-step 10))
-  ((>= i 1400))
+  ((> i 1400))
   (begin
+    (newline)
     (display (format #f "Time : ~04d" (* i ctime-step)))
     (newline)
     (read-data (format #f "~a-~04d" casename i))
     (hy-export-xy-plot (format #f "th~04d.xy" i) zoneid)
     (hy-write-list-tec-file
       (format #f "th~04d.dat" i)
-      (hy-read-xyplot-file (format #f "th~04d.xy" i) (* i ctime-step))
+      (hy-read-xyplot-file (format #f "th~04d.xy" i))
+      (* i ctime-step)
     )
   )
 )
